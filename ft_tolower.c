fc471971e16eaec1d0fe87299e69c9258005ca5a
Pedro De carvalho kumbis chinelli <pde-carv@c2r1p4.42sp.org.br>
2020-03-11 13:26:42 -0300

    documentação de ft_tolower e ft_toupper

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,12 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pde-carv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pde-carv <pde-carv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 12:17:21 by pde-carv          #+#    #+#             */
-/*   Updated: 2020/01/29 12:40:47 by pde-carv         ###   ########.fr       */
+/*   Updated: 2020/03/11 13:26:04 by pde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**	Converts uppercase characters to lowercase ones
+**
+**	Variables
+**	---------
+**	int c: the character to be converted
+*/
 
 int	ft_tolower(int c)
 {
--- ft_toupper.c
+++ ft_toupper.c
@@ -3,12 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pde-carv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pde-carv <pde-carv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 11:40:18 by pde-carv          #+#    #+#             */
-/*   Updated: 2020/01/29 12:40:34 by pde-carv         ###   ########.fr       */
+/*   Updated: 2020/03/11 13:25:55 by pde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**	Converts lowercase characters to uppercase ones
+**
+**	Variables
+**	---------
+**	int c: the character to be converted
+*/
 
 int	ft_toupper(int c)
 {
