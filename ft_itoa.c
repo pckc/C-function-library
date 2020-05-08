3d3e3b415eb52e244dc63593aac3ffe29bc85be6
pckc <pckc@DESKTOP-PEDRO.localdomain>
2020-05-08 17:33:28 -0300

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pde-carv <pde-carv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 23:48:26 by pde-carv          #+#    #+#             */
-/*   Updated: 2020/05/07 02:53:27 by pde-carv         ###   ########.fr       */
+/*   Updated: 2020/05/08 17:32:58 by pde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,23 +39,53 @@ char	*ft_itoa(int n)
 		n_cpy = n_cpy / 10;
 		i++;
 	}
-	n_cpy = n;
-	if (n <= 0)
+	if ((n_cpy = n) <= 0)
 		i++;
-	s = (char *)malloc(sizeof(char) * (i + 1));
-	if (s == NULL)
-		return (NULL);
+	if ((s = (char *)malloc(sizeof(char) * (i + 1))) == 0)
+		return (0);
 	s[i] = '\0';
 	while (i--)
 	{
-		s[i] = (n_cpy % 10);
-		s[i] = s[i] * ((n_cpy < 0) ? -1 : 1);
-		s[i] = s[i] + '0';
+		s[i] = ((n_cpy < 0) ? -1 : 1) * (n_cpy % 10) + '0';
 		n_cpy /= 10;
 	}
-	if (n < 0)
-		s[0] = '-';
-	if (n == 0)
-		s[0] = '0';
+	s[0] = (n < 0) ? '-' : s[0];
+	s[0] = (n == 0) ? '0' : s[0];
 	return (s);
 }
+
+/*
+** char	*ft_itoa(int n)
+** {
+** 	char	*s;
+** 	int		i;
+** 	int		n_cpy;
+**
+** 	i = 0;
+** 	n_cpy = n;
+** 	while (n_cpy)
+** 	{
+** 		n_cpy = n_cpy / 10;
+** 		i++;
+** 	}
+** 	n_cpy = n;
+** 	if (n <= 0)
+** 		i++;
+** 	s = (char *)malloc(sizeof(char) * (i + 1));
+** 	if (s == NULL)
+** 		return (NULL);
+** 	s[i] = '\0';
+** 	while (i--)
+** 	{
+** 		s[i] = (n_cpy % 10);
+** 		s[i] = s[i] * ((n_cpy < 0) ? -1 : 1);
+** 		s[i] = s[i] + '0';
+** 		n_cpy /= 10;
+** 	}
+** 	if (n < 0)
+** 		s[0] = '-';
+** 	if (n == 0)
+** 		s[0] = '0';
+** 	return (s);
+** }
+*/
